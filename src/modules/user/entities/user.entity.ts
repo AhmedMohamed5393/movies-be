59e32d45f1bf8fd2139513d20464c58ef2c56c2d
AhmedMohamed5393 <mody539325@gmail.com>
2025-05-01 13:35:16 +0300

    add relations between users and other tables

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,6 +1,9 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
 import { Exclude } from 'class-transformer';
 import { Base } from '@shared/entities/base.entity';
+import { Movie } from 'src/modules/movie/entities/movie.entity';
+import { Rating } from 'src/modules/rating/entities/rating.entity';
+import { WatchListItem } from 'src/modules/watchlist/entities/wishlist.entity';
 
 @Entity({ name: 'users' })
 export class User extends Base {
@@ -10,4 +13,14 @@ export class User extends Base {
 
   @Column({ type: 'varchar', length: 100, unique: true })
   email: string;
+
+  @OneToMany(() => Movie, (movie) => movie.poster, { cascade: true })
+  movies: Movie[];
+
+  @OneToMany(() => Rating, (rating) => rating.user, { cascade: true })
+  ratings: Rating[];
+
+  @Exclude()
+  @OneToMany(() => WatchListItem, (watchListItem) => watchListItem.user)
+  watchListItems: WatchListItem[];
 }
